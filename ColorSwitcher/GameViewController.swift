4655f6ec62e4d69f1e29c204a7ab2dd89fd9daf6
himchawla <58020143+himchawla@users.noreply.github.com>
2021-05-20 11:03:01 +1200

    shape

--- ColorSwitcher/GameViewController.swift
+++ ColorSwitcher/GameViewController.swift
@@ -5,45 +5,32 @@
 //  Created by Himanshu Chawla on 20/05/21.
 //
 
-import UIKit
-import SpriteKit
-import GameplayKit
+import UIKit;
+import SpriteKit;
+import GameplayKit;
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFill;
                 
                 // Present the scene
-                view.presentScene(scene)
+                view.presentScene(scene);
             }
             
-            view.ignoresSiblingOrder = true
+            view.ignoresSiblingOrder = true;
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = true;
+            view.showsNodeCount = true;
+            
+            //view.showsPhysics
         }
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
